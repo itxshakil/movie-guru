eb20f97e7f9391567c543a7e08df75111da4a6fd
justconsult-dev <113089050+shakil-jc@users.noreply.github.com>
2024-02-03 16:00:05 +0530

    Update user authentication logic to use bcrypt for password hashing
    Signed-off-by: justconsult-dev <113089050+shakil-jc@users.noreply.github.com>

--- resources/js/Components/OurFeatures.vue
+++ resources/js/Components/OurFeatures.vue
@@ -1,67 +1,207 @@
 <template>
-  <div class="bg-white dark:bg-gray-900 dark:text-white py-24 sm:py-32">
-    <div class="mx-auto max-w-7xl px-2 lg:px-8">
-      <div class="mx-auto max-w-2xl lg:text-center">
-        <h2 class="text-base font-semibold leading-7 text-primary-600 dark:text-primary-500">One stop binge hunt solution</h2>
-        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">Everything you need for your binge hunt</p>
-        <p class="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-400">Welcome to Movie Guru, the open movie database that offers a seamless movie-watching experience with a range of features that make it a great alternative to traditional movie databases like IMDB.</p>
-      </div>
-      <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-        <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-          <div class="relative pl-16">
-            <dt class="text-base font-semibold leading-7 text-gray-900 dark:text-white">
-              <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary-600">
-                <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                     aria-hidden="true">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17.657 12.007a1.39 1.39 0 0 0 -1.103 .765l-.855 1.723l-1.907 .277c-.52 .072 -.96 .44 -1.124 .944l-.038 .14c-.1 .465 .046 .954 .393 1.29l1.377 1.337l-.326 1.892a1.393 1.393 0 0 0 2.018 1.465l1.708 -.895l1.708 .896a1.388 1.388 0 0 0 1.462 -.105l.112 -.09a1.39 1.39 0 0 0 .442 -1.272l-.325 -1.891l1.38 -1.339c.38 -.371 .516 -.924 .352 -1.427l-.051 -.134a1.39 1.39 0 0 0 -1.073 -.81l-1.907 -.278l-.853 -1.722a1.393 1.393 0 0 0 -1.247 -.773l-.143 .007z" stroke-width="0" fill="currentColor" /><path d="M6.057 12.007a1.39 1.39 0 0 0 -1.103 .765l-.855 1.723l-1.907 .277c-.52 .072 -.96 .44 -1.124 .944l-.038 .14c-.1 .465 .046 .954 .393 1.29l1.377 1.337l-.326 1.892a1.393 1.393 0 0 0 2.018 1.465l1.708 -.895l1.708 .896a1.388 1.388 0 0 0 1.462 -.105l.112 -.09a1.39 1.39 0 0 0 .442 -1.272l-.324 -1.891l1.38 -1.339c.38 -.371 .516 -.924 .352 -1.427l-.051 -.134a1.39 1.39 0 0 0 -1.073 -.81l-1.908 -.279l-.853 -1.722a1.393 1.393 0 0 0 -1.247 -.772l-.143 .007z" stroke-width="0" fill="currentColor" /><path d="M11.857 2.007a1.39 1.39 0 0 0 -1.103 .765l-.855 1.723l-1.907 .277c-.52 .072 -.96 .44 -1.124 .944l-.038 .14c-.1 .465 .046 .954 .393 1.29l1.377 1.337l-.326 1.892a1.393 1.393 0 0 0 2.018 1.465l1.708 -.894l1.709 .896a1.388 1.388 0 0 0 1.462 -.105l.112 -.09a1.39 1.39 0 0 0 .442 -1.272l-.325 -1.892l1.38 -1.339c.38 -.371 .516 -.924 .352 -1.427l-.051 -.134a1.39 1.39 0 0 0 -1.073 -.81l-1.908 -.279l-.853 -1.722a1.393 1.393 0 0 0 -1.247 -.772l-.143 .007z" stroke-width="0" fill="currentColor" />
-                </svg>
-              </div>
-                Ratings from All Major Sources in One Place
-            </dt>
-            <dd class="mt-2 text-base leading-7 text-gray-600 dark:text-gray-400">Get comprehensive movie ratings from all major sources consolidated in one location. Make informed decisions about what to watch based on a collective perspective.
-            </dd>
-          </div>
-          <div class="relative pl-16">
-            <dt class="text-base font-semibold leading-7 text-gray-900 dark:text-white">
-              <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary-600">
-                <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                     aria-hidden="true">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" /><circle cx="12" cy="7.5" r=".5" fill="currentColor" />
-                </svg>
-              </div>
-                Simple UI
-            </dt>
-            <dd class="mt-2 text-base leading-7 text-gray-600 dark:text-gray-400">Enjoy an intuitive and straightforward user interface that makes navigating through the vast world of movies a breeze. No clutter, no confusion—just a clean and user-friendly design.
-            </dd>
-          </div>
-          <div class="relative pl-16">
-            <dt class="text-base font-semibold leading-7 text-gray-900 dark:text-white">
-              <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary-600">
-                  <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                     aria-hidden="true">
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20l10 -10m0 -5v5h5m-9 -1v5h5m-9 -1v5h5m-5 -5l4 -4l4 -4" /><path d="M19 10c.638 -.636 1 -1.515 1 -2.486a3.515 3.515 0 0 0 -3.517 -3.514c-.97 0 -1.847 .367 -2.483 1m-3 13l4 -4l4 -4" />
-                  </svg>
-              </div>
-                Lightweight
-            </dt>
-            <dd class="mt-2 text-base leading-7 text-gray-600 dark:text-gray-400">Movie Guru is designed to be light on resources, ensuring a swift and efficient experience. Access information about your favorite movies without any unnecessary lags or delays.
-            </dd>
-          </div>
-          <div class="relative pl-16">
-            <dt class="text-base font-semibold leading-7 text-gray-900 dark:text-white">
-              <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary-600">
-                <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                     aria-hidden="true">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 4l0 16" /><path d="M16 4l0 16" /><path d="M4 8l4 0" /><path d="M4 16l4 0" /><path d="M4 12l16 0" /><path d="M16 8l4 0" /><path d="M16 16l4 0" />
-                </svg>
-              </div>
-                An Alternative to IMDB
-            </dt>
-            <dd class="mt-2 text-base leading-7 text-gray-600 dark:text-gray-400">Looking for a fresh perspective? Movie Guru provides an alternative to the traditional IMDB platform, offering a new way to explore and discover your favorite films.
-            </dd>
-          </div>
-        </dl>
-      </div>
+    <div class="bg-white dark:bg-gray-900 dark:text-white py-24 sm:py-32">
+        <div class="mx-auto max-w-7xl px-4 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:text-center">
+                <h2
+                    class="text-base font-semibold leading-7 text-primary-600 dark:text-primary-500"
+                >
+                    One stop binge hunt solution
+                </h2>
+                <p
+                    class="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl"
+                >
+                    Everything you need for your binge hunt
+                </p>
+                <p
+                    class="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-400"
+                >
+                    Welcome to Movie Guru, the open movie database that offers a
+                    seamless movie-watching experience with a range of features
+                    that make it a great alternative to traditional movie
+                    databases like IMDB.
+                </p>
+            </div>
+            <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+                <dl
+                    class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16"
+                >
+                    <div class="relative pl-16">
+                        <dt
+                            class="text-base font-semibold leading-7 text-gray-900 dark:text-white"
+                        >
+                            <div
+                                class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary-600"
+                            >
+                                <svg
+                                    class="h-6 w-6 text-white"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        stroke="none"
+                                        d="M0 0h24v24H0z"
+                                        fill="none"
+                                    />
+                                    <path
+                                        d="M17.657 12.007a1.39 1.39 0 0 0 -1.103 .765l-.855 1.723l-1.907 .277c-.52 .072 -.96 .44 -1.124 .944l-.038 .14c-.1 .465 .046 .954 .393 1.29l1.377 1.337l-.326 1.892a1.393 1.393 0 0 0 2.018 1.465l1.708 -.895l1.708 .896a1.388 1.388 0 0 0 1.462 -.105l.112 -.09a1.39 1.39 0 0 0 .442 -1.272l-.325 -1.891l1.38 -1.339c.38 -.371 .516 -.924 .352 -1.427l-.051 -.134a1.39 1.39 0 0 0 -1.073 -.81l-1.907 -.278l-.853 -1.722a1.393 1.393 0 0 0 -1.247 -.773l-.143 .007z"
+                                        stroke-width="0"
+                                        fill="currentColor"
+                                    />
+                                    <path
+                                        d="M6.057 12.007a1.39 1.39 0 0 0 -1.103 .765l-.855 1.723l-1.907 .277c-.52 .072 -.96 .44 -1.124 .944l-.038 .14c-.1 .465 .046 .954 .393 1.29l1.377 1.337l-.326 1.892a1.393 1.393 0 0 0 2.018 1.465l1.708 -.895l1.708 .896a1.388 1.388 0 0 0 1.462 -.105l.112 -.09a1.39 1.39 0 0 0 .442 -1.272l-.324 -1.891l1.38 -1.339c.38 -.371 .516 -.924 .352 -1.427l-.051 -.134a1.39 1.39 0 0 0 -1.073 -.81l-1.908 -.279l-.853 -1.722a1.393 1.393 0 0 0 -1.247 -.772l-.143 .007z"
+                                        stroke-width="0"
+                                        fill="currentColor"
+                                    />
+                                    <path
+                                        d="M11.857 2.007a1.39 1.39 0 0 0 -1.103 .765l-.855 1.723l-1.907 .277c-.52 .072 -.96 .44 -1.124 .944l-.038 .14c-.1 .465 .046 .954 .393 1.29l1.377 1.337l-.326 1.892a1.393 1.393 0 0 0 2.018 1.465l1.708 -.894l1.709 .896a1.388 1.388 0 0 0 1.462 -.105l.112 -.09a1.39 1.39 0 0 0 .442 -1.272l-.325 -1.892l1.38 -1.339c.38 -.371 .516 -.924 .352 -1.427l-.051 -.134a1.39 1.39 0 0 0 -1.073 -.81l-1.908 -.279l-.853 -1.722a1.393 1.393 0 0 0 -1.247 -.772l-.143 .007z"
+                                        stroke-width="0"
+                                        fill="currentColor"
+                                    />
+                                </svg>
+                            </div>
+                            Ratings from All Major Sources in One Place
+                        </dt>
+                        <dd
+                            class="mt-2 text-base leading-7 text-gray-600 dark:text-gray-400"
+                        >
+                            Get comprehensive movie ratings from all major
+                            sources consolidated in one location. Make informed
+                            decisions about what to watch based on a collective
+                            perspective.
+                        </dd>
+                    </div>
+                    <div class="relative pl-16">
+                        <dt
+                            class="text-base font-semibold leading-7 text-gray-900 dark:text-white"
+                        >
+                            <div
+                                class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary-600"
+                            >
+                                <svg
+                                    class="h-6 w-6 text-white"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        stroke="none"
+                                        d="M0 0h24v24H0z"
+                                        fill="none"
+                                    />
+                                    <path
+                                        d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"
+                                    />
+                                    <path
+                                        d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1"
+                                    />
+                                    <circle
+                                        cx="12"
+                                        cy="7.5"
+                                        r=".5"
+                                        fill="currentColor"
+                                    />
+                                </svg>
+                            </div>
+                            Simple UI
+                        </dt>
+                        <dd
+                            class="mt-2 text-base leading-7 text-gray-600 dark:text-gray-400"
+                        >
+                            Enjoy an intuitive and straightforward user
+                            interface that makes navigating through the vast
+                            world of movies a breeze. No clutter, no
+                            confusion—just a clean and user-friendly design.
+                        </dd>
+                    </div>
+                    <div class="relative pl-16">
+                        <dt
+                            class="text-base font-semibold leading-7 text-gray-900 dark:text-white"
+                        >
+                            <div
+                                class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary-600"
+                            >
+                                <svg
+                                    class="h-6 w-6 text-white"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        stroke="none"
+                                        d="M0 0h24v24H0z"
+                                        fill="none"
+                                    />
+                                    <path
+                                        d="M4 20l10 -10m0 -5v5h5m-9 -1v5h5m-9 -1v5h5m-5 -5l4 -4l4 -4"
+                                    />
+                                    <path
+                                        d="M19 10c.638 -.636 1 -1.515 1 -2.486a3.515 3.515 0 0 0 -3.517 -3.514c-.97 0 -1.847 .367 -2.483 1m-3 13l4 -4l4 -4"
+                                    />
+                                </svg>
+                            </div>
+                            Lightweight
+                        </dt>
+                        <dd
+                            class="mt-2 text-base leading-7 text-gray-600 dark:text-gray-400"
+                        >
+                            Movie Guru is designed to be light on resources,
+                            ensuring a swift and efficient experience. Access
+                            information about your favorite movies without any
+                            unnecessary lags or delays.
+                        </dd>
+                    </div>
+                    <div class="relative pl-16">
+                        <dt
+                            class="text-base font-semibold leading-7 text-gray-900 dark:text-white"
+                        >
+                            <div
+                                class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary-600"
+                            >
+                                <svg
+                                    class="h-6 w-6 text-white"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        stroke="none"
+                                        d="M0 0h24v24H0z"
+                                        fill="none"
+                                    />
+                                    <path
+                                        d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"
+                                    />
+                                    <path d="M8 4l0 16" />
+                                    <path d="M16 4l0 16" />
+                                    <path d="M4 8l4 0" />
+                                    <path d="M4 16l4 0" />
+                                    <path d="M4 12l16 0" />
+                                    <path d="M16 8l4 0" />
+                                    <path d="M16 16l4 0" />
+                                </svg>
+                            </div>
+                            An Alternative to IMDB
+                        </dt>
+                        <dd
+                            class="mt-2 text-base leading-7 text-gray-600 dark:text-gray-400"
+                        >
+                            Looking for a fresh perspective? Movie Guru provides
+                            an alternative to the traditional IMDB platform,
+                            offering a new way to explore and discover your
+                            favorite films.
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
