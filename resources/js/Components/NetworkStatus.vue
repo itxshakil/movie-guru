b523471d92cb5b907242d7cac23d784eebe7cd4d
itxshakil <itxshakil@gmail.com>
2024-02-17 20:12:59 +0530

    Add Network Status component
    Signed-off-by: itxshakil <itxshakil@gmail.com>

--- resources/js/Components/NetworkStatus.vue
+++ resources/js/Components/NetworkStatus.vue
@@ -1,27 +1,22 @@
 <script setup>
-import Snackbar from "@/Components/Snackbar.vue";
 import { ref, onMounted, onUnmounted, computed } from "vue";
 
-const promptNotify = ref(false);
+const show = ref(false);
 const onlineStatus = ref(navigator.onLine);
-const promptTitle = ref("Network Status");
-const primaryAction = ref(null);
-const secondaryAction = ref("Okay");
-
-const statusMessage = computed(() => {
-    return onlineStatus.value
-        ? "You are now online."
-        : "You are currently offline. Enable notifications to be notified when the network is available.";
-});
+const message = ref("You are now online");
 
 const updateOnlineStatus = () => {
     onlineStatus.value = navigator.onLine;
     if (onlineStatus.value) {
-        promptNotify.value = true;
-        primaryAction.value = null;
+        show.value = true;
+        message.value = "You are now online";
+
+        setTimeout(() => {
+            show.value = false;
+        }, 1000);
     } else {
-        promptNotify.value = true;
-        primaryAction.value = "Notify Me";
+        show.value = true;
+        message.value = "You are now offline";
     }
 };
 
@@ -35,20 +30,34 @@ onUnmounted(() => {
     window.removeEventListener("offline", updateOnlineStatus);
 });
 
-const cancelPrompt = () => {
-    promptNotify.value = false;
-};
+const classes = computed(() => {
+    return {
+        "bg-green-600": onlineStatus.value,
+        "bg-red-600": !onlineStatus.value,
+    };
+});
 </script>
 
 <template>
-    <Snackbar
-        v-if="promptNotify"
-        :title="promptTitle"
-        :description="statusMessage"
-        :primaryAction="primaryAction"
-        :secondaryAction="secondaryAction"
-        @close="cancelPrompt"
-        @confirm="cancelPrompt"
-        :delay="100"
-    />
+    <transition
+        enter-active-class="duration-500 ease-out"
+        enter-from-class="opacity-0 transform translate-y-2"
+        enter-to-class="opacity-100 transform translate-y-0"
+        leave-active-class="duration-500 ease-in"
+        leave-from-class="opacity-100 transform translate-y-0"
+        leave-to-class="opacity-0 transform translate-y-2"
+    >
+        <div
+            v-if="show"
+            class="w-full p-2 text-gray-100 shadow-md fixed bottom-0 right-0 z-10"
+            :class="classes"
+            role="alert"
+        >
+            <div class="flex justify-center">
+                <span class="text-sm tracking-wider">
+                    {{ message }}
+                </span>
+            </div>
+        </div>
+    </transition>
 </template>
