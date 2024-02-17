129d8a02728350b8249ca525e4991f26abb9a87b
itxshakil <itxshakil@gmail.com>
2024-02-17 20:33:10 +0530

    Stack snackbar
    Signed-off-by: itxshakil <itxshakil@gmail.com>

--- resources/js/Layouts/BaseLayout.vue
+++ resources/js/Layouts/BaseLayout.vue
@@ -1,20 +1,24 @@
 <script setup>
-import AppFooter from '@/Layouts/AppFooter.vue';
-import ToastNotifications from '@/Components/ToastNotifications.vue';
-import InstallBanner from '@/Components/InstallBanner.vue';
-import AppHeader from '@/Layouts/AppHeader.vue';
-import OfflineSearchPrompt from '@/Components/OfflineSearchPrompt.vue';
-import NetworkStatus from '@/Components/NetworkStatus.vue';
-
+import AppFooter from "@/Layouts/AppFooter.vue";
+import ToastNotifications from "@/Components/ToastNotifications.vue";
+import InstallBanner from "@/Components/InstallBanner.vue";
+import AppHeader from "@/Layouts/AppHeader.vue";
+import OfflineSearchPrompt from "@/Components/OfflineSearchPrompt.vue";
+import NetworkStatus from "@/Components/NetworkStatus.vue";
 </script>
 
 <template>
-    <AppHeader/>
+    <AppHeader />
     <slot />
-    <AppFooter/>
+    <AppFooter />
     <ToastNotifications />
+    <InstallBanner />
     <InstallBanner />
     <OfflineSearchPrompt />
     <NetworkStatus />
+
+    <div
+        class="fixed bottom-0 right-0 z-50 flex flex-col snackbar-container mb-2"
+    ></div>
     <slot name="modals" />
 </template>
