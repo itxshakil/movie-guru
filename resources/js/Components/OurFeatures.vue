798da922dc883ec5d39c0614092f2b72e80a2a7d
Nargis Khatun <itxnargiskhatun@gmail.com>
2024-05-05 13:06:14 +0530

    Move Alternative to Imdb to TOp
    Signed-off-by: Nargis Khatun <itxnargiskhatun@gmail.com>

--- resources/js/Components/OurFeatures.vue
+++ resources/js/Components/OurFeatures.vue
@@ -46,6 +46,49 @@
                                         fill="none"
                                     />
                                     <path
+                                        d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"
+                                    />
+                                    <path d="M8 4l0 16" />
+                                    <path d="M16 4l0 16" />
+                                    <path d="M4 8l4 0" />
+                                    <path d="M4 16l4 0" />
+                                    <path d="M4 12l16 0" />
+                                    <path d="M16 8l4 0" />
+                                    <path d="M16 16l4 0" />
+                                </svg>
+                            </div>
+                            An Alternative to IMDB
+                        </dt>
+                        <dd
+                            class="mt-2 text-base leading-7 text-gray-600 dark:text-gray-400"
+                        >
+                            Looking for a fresh perspective? Movie Guru provides
+                            an alternative to the traditional IMDB platform,
+                            offering a new way to explore and discover your
+                            favorite films.
+                        </dd>
+                    </div>
+                    <div class="relative pl-16">
+                        <dt
+                            class="text-base font-semibold leading-7 text-gray-900 dark:text-white"
+                        >
+                            <div
+                                class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary-600"
+                            >
+                                <svg
+                                    class="h-6 w-6 text-white"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        stroke="none"
+                                        d="M0 0h24v24H0z"
+                                        fill="none"
+                                    />
+                                    <path
                                         d="M17.657 12.007a1.39 1.39 0 0 0 -1.103 .765l-.855 1.723l-1.907 .277c-.52 .072 -.96 .44 -1.124 .944l-.038 .14c-.1 .465 .046 .954 .393 1.29l1.377 1.337l-.326 1.892a1.393 1.393 0 0 0 2.018 1.465l1.708 -.895l1.708 .896a1.388 1.388 0 0 0 1.462 -.105l.112 -.09a1.39 1.39 0 0 0 .442 -1.272l-.325 -1.891l1.38 -1.339c.38 -.371 .516 -.924 .352 -1.427l-.051 -.134a1.39 1.39 0 0 0 -1.073 -.81l-1.907 -.278l-.853 -1.722a1.393 1.393 0 0 0 -1.247 -.773l-.143 .007z"
                                         stroke-width="0"
                                         fill="currentColor"
@@ -155,49 +198,6 @@
                             ensuring a swift and efficient experience. Access
                             information about your favorite movies without any
                             unnecessary lags or delays.
-                        </dd>
-                    </div>
-                    <div class="relative pl-16">
-                        <dt
-                            class="text-base font-semibold leading-7 text-gray-900 dark:text-white"
-                        >
-                            <div
-                                class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary-600"
-                            >
-                                <svg
-                                    class="h-6 w-6 text-white"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                    aria-hidden="true"
-                                >
-                                    <path
-                                        stroke="none"
-                                        d="M0 0h24v24H0z"
-                                        fill="none"
-                                    />
-                                    <path
-                                        d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"
-                                    />
-                                    <path d="M8 4l0 16" />
-                                    <path d="M16 4l0 16" />
-                                    <path d="M4 8l4 0" />
-                                    <path d="M4 16l4 0" />
-                                    <path d="M4 12l16 0" />
-                                    <path d="M16 8l4 0" />
-                                    <path d="M16 16l4 0" />
-                                </svg>
-                            </div>
-                            An Alternative to IMDB
-                        </dt>
-                        <dd
-                            class="mt-2 text-base leading-7 text-gray-600 dark:text-gray-400"
-                        >
-                            Looking for a fresh perspective? Movie Guru provides
-                            an alternative to the traditional IMDB platform,
-                            offering a new way to explore and discover your
-                            favorite films.
                         </dd>
                     </div>
                 </dl>
