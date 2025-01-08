20177e3abcc33efbe9ca72d49b60d1780a018f83
Shakil Alam <itxshakil@gmail.com>
2025-01-08 21:05:26 +0530

    fix: update meta tag for mobile web app capability
    Replaced "apple-mobile-web-app-capable" with "mobile-web-app-capable" for broader compatibility. Ensures consistency across different mobile platforms.
    
    Signed-off-by: Shakil Alam <itxshakil@gmail.com>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -38,7 +38,7 @@
     <link rel="icon" href="/favicon.ico" type="image/x-icon" />
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
     <link rel="apple-touch-icon" href="/icons/ios/192.png">
-    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-title" content="Movie Guru">
     <link rel="manifest" href="/app.webmanifest" />
     <script type="application/ld+json">
