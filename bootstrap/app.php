2a0ac4bfafdda2fdfea2e34bb069d37c4c85eadd
Shakil Alam <itxshakil@gmail.com>
2025-01-11 18:36:06 +0530

    chore: Basic formatting
    Signed-off-by: Shakil Alam <itxshakil@gmail.com>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,11 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append([
-            HandleInertiaRequests::class
+            HandleInertiaRequests::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->respond(function(Response $response, Throwable $exception, Request $request) {
+        $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
             if (! app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
                 return Inertia::render('Error', ['status' => $response->getStatusCode()])
                     ->toResponse($request)
