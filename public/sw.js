5c9dca26151d12ea2cd22541a729d0f68dca1c10
Shakil Alam <30234430+itxshakil@users.noreply.github.com>
2026-01-25 00:17:44 +0530

    chore: Update SW
    Signed-off-by: Shakil Alam <30234430+itxshakil@users.noreply.github.com>

--- public/sw.js
+++ public/sw.js
@@ -2,11 +2,11 @@ const DEBUG = false;
 const broadcast = new BroadcastChannel('service-worker-channel');
 const broadcastChannel = new BroadcastChannel('toast-notifications');
 
-const APP_CACHE = 'v-4.10.1';
-const SEARCH_CACHE = 'search-cache-v-4.10.1';
-const INFO_CACHE = 'info-cache-v-4.10.1';
-const DYNAMIC_CACHE = 'dynamic-cache-v-10.1';
-const POSTER_CACHE = 'poster-cache-v-4.10.1';
+const APP_CACHE = 'v-4.11.0';
+const SEARCH_CACHE = 'search-cache-v-4.11.0';
+const INFO_CACHE = 'info-cache-v-4.11.0';
+const DYNAMIC_CACHE = 'dynamic-cache-v-11.0';
+const POSTER_CACHE = 'poster-cache-v-4.11.0';
 
 const STATIC_ASSETS = [
     '/app.webmanifest',
